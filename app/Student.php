c7f71e83b3b2f01ec455b2eff9f22d51c4866bf2
azharhamdani8 <azhar.153040054@mail.unpas.ac.id>
2019-12-03 02:05:35 +0700

    menambahkan fiture soft daletes

--- app/Student.php
+++ app/Student.php
@@ -3,8 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\softDeletes;
 class Student extends Model
 {
+    use softDeletes;
     protected $fillable = ['nama','nrp','email','jurusan'];
 }
--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,13 @@ Route::get('/', 'PageController@home');
 Route::get('/about', 'PageController@about');
 Route::get('/mahasiswa', 'MahasiswaController@index');
 
-Route::get('/students', 'StudentsController@index');
-Route::get('/students/create', 'StudentsController@create');
-Route::get('/students/{student}', 'StudentsController@show');
-Route::post('/students', 'StudentsController@store');
-Route::delete('/students/{student}', 'StudentsController@destroy');
-Route::get('/students/{student}/edit', 'StudentsController@edit');
-Route::patch('/students/{student}/', 'StudentsController@update');
+// Route::get('/students', 'StudentsController@index');
+// Route::get('/students/create', 'StudentsController@create');
+// Route::get('/students/{student}', 'StudentsController@show');
+// Route::post('/students', 'StudentsController@store');
+// Route::delete('/students/{student}', 'StudentsController@destroy');
+// Route::get('/students/{student}/edit', 'StudentsController@edit');
+// Route::patch('/students/{student}/', 'StudentsController@update');
 
+Route::resource('students', 'StudentsController');
 
